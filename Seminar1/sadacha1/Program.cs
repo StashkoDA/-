6b81d96a659acbc8e529ff0fd9147b2c8fb7a880
Dmitrii_Stashko <1028047@mail.ru>
2022-09-01 09:52:11 +1000

    Создал папку Seminar1

---
+++ Seminar1/sadacha1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите целое число ");
+int x = Convert.ToInt32 (Console.ReadLine());
+int y = x*x;
+Console.WriteLine("Квадрат этого числа равен "+y);
+if (x>0)
+{
+    Console.WriteLine("Число положительное");
+}
+else if (x<0) Console.WriteLine("Число отрицательное");
+else Console.WriteLine("Вы ввели ноль");
