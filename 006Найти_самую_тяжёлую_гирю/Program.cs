8f5276ed14cc7af0e355fa0c8bb81fed12096720
Dmitrii_Stashko <1028047@mail.ru>
2022-08-31 17:10:49 +1000

    Добавил проект 006Найти_самую_тяжёлую_гирю

---
+++ 006Найти_самую_тяжёлую_гирю/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 1;
+int b = 10;
+int c = 8;
+int d = 5;
+int e = 100;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (d > max) max = d;
+if (c > max) max = c;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
