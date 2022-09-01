f2791646bd485b2447455a8dbb0529fdaa121d3f
Dmitrii_Stashko <1028047@mail.ru>
2022-09-01 11:45:30 +1000

    Добавил Sadacha2 в папку Seminar1

--- Seminar1/sadacha2/Program.cs
+++ Seminar1/sadacha2/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите целое число a и b");
+int a = Convert.ToInt32 (Console.ReadLine());
+int b = Convert.ToInt32 (Console.ReadLine());
+int c = b*b;
+Console.WriteLine("Квадрат числа b "+c);
+if (c==a) Console.WriteLine("a квадрат числа b");
+else if (c<a) Console.WriteLine("a не квадрат числа b");
+else if (c>a) Console.WriteLine("a не квадрат числа b");
