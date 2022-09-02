a2c5334550a1df176103a6e50ede019565e64b9b
Dmitrii_Stashko <1028047@mail.ru>
2022-09-02 16:05:14 +1000

    Корректировка Sadacha3 в папкt Seminar1

--- Seminar1/Sadacha3/Program.cs
+++ Seminar1/Sadacha3/Program.cs
@@ -1,12 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.Write("Введите целое число от 1 до 7 = ");
-int x = Convert.ToInt32 (Console.ReadLine());
-if (x==1) Console.WriteLine("Это понедельник");
-else if (x==2) Console.WriteLine("Это вторник");
-else if (x==3) Console.WriteLine("Это среда");
-else if (x==4) Console.WriteLine("Это четверг");
-else if (x==5) Console.WriteLine("Это пятница");
-else if (x==6) Console.WriteLine("Это суббота");
-else if (x==7) Console.WriteLine("Это воскресение");
-else if (x<1) Console.WriteLine("Вы ввели число меньше 1");
-else if (x>7) Console.WriteLine("Вы ввели число больше 7");
+try
+    {
+    int x = Convert.ToInt32 (Console.ReadLine());
+    if (x==1) Console.WriteLine("Это понедельник");
+    else if (x==2) Console.WriteLine("Это вторник");
+    else if (x==3) Console.WriteLine("Это среда");
+    else if (x==4) Console.WriteLine("Это четверг");
+    else if (x==5) Console.WriteLine("Это пятница");
+    else if (x==6) Console.WriteLine("Это суббота");
+    else if (x==7) Console.WriteLine("Это воскресение");
+    else if (x<1) Console.WriteLine("Вы ввели число меньше 1");
+    else if (x>7) Console.WriteLine("Вы ввели число больше 7");
+    }
+catch
+{
+    Console.WriteLine("Надо было ввести целое число");
+}
