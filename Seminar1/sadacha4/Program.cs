157acf6afa5ab8211eb0c2b6e8257199615d90d1
Dmitrii_Stashko <1028047@mail.ru>
2022-09-02 16:47:23 +1000

    Добавил Sadacha4 в папку Seminar1

---
+++ Seminar1/sadacha4/Program.cs
@@ -0,0 +1,33 @@
+﻿//  Напишите программу, которая на вход принимает одно число (N),
+// а на выходе показывает все целые числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> " -2, -1, 0, 1, 2"
+Console.Write("Введите целое число ");
+try
+    {
+        int x = Convert.ToInt32 (Console.ReadLine());
+        if (x>0)
+        {
+            int i = -x;
+            while (i<=x)
+            {
+                Console.Write(i+", ");
+                i++;
+            }
+        }
+        else if (x<0)
+        {
+            int i = x;
+            while (i<=-x)
+            {
+                Console.Write(i+", ");
+                i++;
+            }
+        }
+        else Console.WriteLine("Вы ввели ноль");
+    Console.WriteLine("Спасибо");
+    }
+catch
+{
+    Console.WriteLine("Надо было ввести целое число");
+}   
