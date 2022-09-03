22d0e068924f5570382ce4e0856bab5275f63135
Dmitrii_Stashko <1028047@mail.ru>
2022-09-03 12:31:36 +1000

    Добавил задачу Poisk_find

---
+++ Lesson2/Poisk_find/Program.cs
@@ -0,0 +1,17 @@
+﻿// В одномерном массиве array  из n  элементов найти эл-т массива, равный find (поиск индекса элемента).
+
+int[] array = {15, 21, 39, 12, 23, 33, 123, 333, 23};
+int n = array.Length; //задали длину массива.
+int find = 23;
+int index = 0;
+while (index < n)
+{
+    if (array [index] == find)
+    {
+        Console.WriteLine(index);
+        break; //прервать операцию при наждении первого эл-та, когда имеются одинаковые (23).
+    }
+    //index = index +1;
+    index++;
+}
+
