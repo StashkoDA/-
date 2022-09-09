403234fdb33512d9e37fbc893778eea73a3eb940
Dmitrii_Stashko <1028047@mail.ru>
2022-09-09 21:57:21 +1000

    Добавил Zadacha3 в папку DZ

---
+++ Seminar2/DZ/Zadacha3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+// 6 -> да; 7 -> да; 1 -> нет
+
+try
+{
+    void Day(int numb)
+    {
+        if (numb > 0 & numb < 8)
+        {
+            if (numb == 6) Console.WriteLine("Это выходной");
+            else if (numb == 7) Console.WriteLine("Это выходной");
+            else Console.WriteLine("Это не выходной");
+        }
+        else Console.WriteLine("Введите целое число от 1 до 7");
+    }
+    Console.WriteLine("Введите целое число от 1 до 7");
+    int numb = Convert.ToInt32 (Console.ReadLine());
+    Day(numb);
+    Console.WriteLine();
+}
+catch
+{
+Console.WriteLine("Введите целое число от 1 до 7");
+}
