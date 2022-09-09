4358f4ee93e789292f583fbe12154ccbb58539f8
Dmitrii_Stashko <1028047@mail.ru>
2022-09-09 16:17:45 +1000

    Создал папку DZ + Zadacha1

---
+++ Seminar2/DZ/Zadacha1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и
+// на выходе показывает вторую цифру этого числа.
+// 456 -> 5; 782 -> 8; 918 -> 1
+
+try
+{
+    void Bum(int numb)
+        {
+            if ( numb > 99 & numb <1000 )
+                {
+                    numb = (numb/10)%10;
+                    Console.WriteLine("Это вторая цифра числа "+numb);
+                }
+             else Console.WriteLine("Надо было ввести трёхзначное число");
+        }
+    Console.Write("Введите трёхзначное число: ");
+    int numb = Convert.ToInt32 (Console.ReadLine());
+    Bum(numb);
+    Console.WriteLine();
+}
+catch
+    {
+        Console.WriteLine("Надо было ввести трёхзначное число");
+    }
