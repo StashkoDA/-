b621d5a098c5d523268a025b3b16e66a4e8dd21c
Dmitrii_Stashko <1028047@mail.ru>
2022-09-10 23:35:07 +1000

    Добавил второе решение Zadacha2_chetvertXY

--- Seminar3/Zadacha2_chetvertXY/Program.cs
+++ Seminar3/Zadacha2_chetvertXY/Program.cs
@@ -3,7 +3,7 @@
 
 string CheckChetvert(int Chetvert) // ввод ф-ции, определяющей четверть плоскости
     {
-        string res;// ввод имени ф-ции для вывода координаты х
+        string res;// ввод имени ф-ции для вывода координат
         if (Chetvert==1) res = "Координаты точки в области {+x,+y}";
         else if (Chetvert==4) res = "Координаты точки в области {+x,-y}";
         else if (Chetvert==3) res = "Координаты точки в области {-x,-y}";
@@ -22,3 +22,40 @@ catch
     {
         Console.WriteLine("Надо было вводить целые числа координат");
     }
+
+// или
+string whatCoord(int num)
+{
+switch(num)
+{
+case 1:
+{
+return "x > 0, y > 0";
+}
+case 2:
+{
+return "x < 0, y > 0";
+}
+case 3:
+{
+return "x < 0, y < 0";
+}
+case 4:
+{
+return "x > 0, y < 0";
+}
+default:
+{
+return "Нет такой четверти!";
+}
+}
+}
+try {
+Console.WriteLine("Введите номер четверти ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(whatCoord(quarter));
+}
+catch
+{
+Console.WriteLine("Нужно ввести целое число");
+}
