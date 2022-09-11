42e22565fcbfab0f31dafd0c025ec43f425dcc44
Dmitrii_Stashko <1028047@mail.ru>
2022-09-11 22:34:21 +1000

    Добавил папку DZ и Zadacha3_Vcub

---
+++ Seminar3/DZ/Zadacha3_Vcub/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 23 - Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27 // 5 -> 1, 8, 27, 64, 125
+
+string KubTable(int num)
+{
+    string res = $"{num} -> ";
+    if (num > 0)
+        {
+            for (int i = 1; i <= num; i++)
+                {
+                    res = res + $" {Math.Pow(i, 3)},"; // {Math.Pow(i, 3)} - это возведение в степень 3
+                }
+        }
+    else if (num < 0)
+        {
+            for (int i = -1; i >= num; i--)
+                {
+                    res = res + $" {Math.Pow(i, 3)},"; // {Math.Pow(i, 3)} - это возведение в степень 3
+
+                }
+        }
+    return res;
+}
+try
+{
+    Console.Write("Введите число = ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    //int num = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+    Console.WriteLine(KubTable(num));
+}
+catch
+{
+    Console.WriteLine("Надо было ввести целое число");
+}
