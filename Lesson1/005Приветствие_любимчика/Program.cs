136cd134bb6231460055fbf9800034a9d6a4e7f4
Dmitrii_Stashko <1028047@mail.ru>
2022-10-04 18:28:31 +1000

    внёс поправки в Lesson1

--- Lesson1/005Приветствие_любимчика/Program.cs
+++ Lesson1/005Приветствие_любимчика/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.Write("Введите имя пользователя: ");
 string username = Console.ReadLine();
 
-if(username.ToLower() == "маша")
+if(username.ToLower() == "маша") //ToLower - позволяет исключить волияние регистра при вводе имени.
 {
     Console.WriteLine("Ура, это же Маша!"); 
 }
