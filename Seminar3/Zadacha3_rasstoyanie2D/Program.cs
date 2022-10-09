c7db1e2bf29ed591771dd10bc17c77d47d51d1f7
Dmitrii_Stashko <1028047@mail.ru>
2022-10-09 14:59:33 +1000

    Seminar3\Zadacha3_rasstoyanie2D добавил вариант решения

--- Seminar3/Zadacha3_rasstoyanie2D/Program.cs
+++ Seminar3/Zadacha3_rasstoyanie2D/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
 // A (3,6); B (2,1) -> 5,09
 // A (7,-5); B (1,-1) -> 7,21
-
+/*
 double Rasst(int xA, int yA, int xB, int yB) // ввод ф-ции, определяющей координаты точки
     {
         double res; // ввод имени ф-ции для вывода результата 
@@ -28,3 +28,39 @@ catch
     {
         Console.WriteLine("Надо было вводить целые числа координат");
     }
+*/
+    /// или
+
+
+
+double GetValue(string text) // функция запроса ввода координат
+{
+    Console.Write(text + ": ");
+    double value = Convert.ToDouble(Console.ReadLine());
+    return value;
+}
+
+void Print(double ax, double ay, double bx, double by, double result) // ф-ция вывода текста ответа
+{
+    // |A(1, 2); B(4, 5)| = 6 - вид изображения координат точек и рассояния м/д ними
+    string output = "|A(" + ax + ", " + ay + "); B(" +
+                            bx + ", " + by + ")| = " +
+                    // result;
+                    Math.Round(result, 2);
+Console.WriteLine(output);
+}
+
+double GetDistance(double ax, double ay, double bx, double by) // ф-ция определения расстояния м/д точками
+{
+    double x = (ax - bx) * (ax - bx); // возведение в квадрат
+    double y = Math.Pow(ay - by, 2); // возведение в квадрат (в степень 2)
+    double result = Math.Sqrt(y + x);
+    return result;
+}
+double ax = GetValue("ax"); // запрос ввода координат
+double ay = GetValue("ay");
+double bx = GetValue("bx");
+double by = GetValue("by");
+
+double dis = GetDistance(ax, ay, bx, by); // запрос ф-ции вычисления расстояния
+Print(ax, ay, bx, by, dis); // вывод на экран координат точек и расстояния м/д ними
