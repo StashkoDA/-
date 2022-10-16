08a82fbc344a3aea3415a4df9ad2cf0bd23c30e2
Dmitrii_Stashko <1028047@mail.ru>
2022-10-16 13:26:55 +1000

    Добавил в DZ решение задачи Z25_stepen

---
+++ Seminar4/DZ/Z25_stepen/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+double GetNumber(string text) // функция запроса ввода координат
+{
+    Console.Write(text + ": ");
+    double number = Convert.ToDouble(Console.ReadLine());
+    return number;
+}
+
+double GetDegree(double a, double b) // ф-ция возведения числа А в степень В
+{
+    double degree = Math.Pow(a, b); // возведение в степень
+    return degree;
+}
+
+void Print(double a, double b, double degree) // ф-ция вывода текста ответа
+{
+    
+    string answer = "Введено число " + a + ", введено число " + b + ", число А в степени В = " + degree;
+  
+Console.WriteLine(answer);
+}
+
+double a = GetNumber("a"); // исходное число
+double b = GetNumber("b"); // значение степени
+double degree = GetDegree(a, b);
+Print(a, b, degree);
+
