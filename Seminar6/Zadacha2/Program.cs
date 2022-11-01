2399a08cecaa5fdd33846d405c24b43812ba028a
Dmitrii_Stashko <1028047@mail.ru>
2022-11-01 22:21:41 +1000

    Добавил задачу Seminar6/Zadacha2

---
+++ Seminar6/Zadacha2/Program.cs
@@ -0,0 +1,16 @@
+﻿int n = 8000;
+int[,] m = new int[n, n];
+int s = 0;
+
+// time start
+DateTime ts = DateTime.Now; // определим время начала операции
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        s += m[i, j];
+    }
+}
+// time end
+DateTime te = DateTime.Now; // определим время окончания операции
+Console.WriteLine((te - ts).TotalMilliseconds);
