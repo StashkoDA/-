1d17c87f56365887a6a3077eba8a5d898b02a78a
Dmitrii_Stashko <1028047@mail.ru>
2022-11-02 19:01:57 +1000

    Добавил задачу Seminar7/DZ/Zadacha2_Sred_arifm_2massiv

---
+++ Seminar7/DZ/Zadacha1_Dvumer_massiv_el/Program.cs
@@ -0,0 +1,85 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+void FillArray(int[,] tabl) // заполняем массив случайными числами из полуинтервала (1, 100]
+{
+    for (int i = 0; i < tabl.GetLength(0); i++)
+    {
+        for (int j = 0; j < tabl.GetLength(1); j++)
+        {
+            tabl[i, j] = new Random().Next(1, 100); // заполняем массив случайными числами из полуинтервала (1, 100]
+        }
+    }
+}
+
+int GetIndex(string text) // функция запроса ввода индекса
+{
+    Console.Write(text + ": ");
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+void GetNumb(int[,] tabl, int numb) // поиск элемента в массиве
+{
+    int[] result = new int[tabl.GetLength(0) * tabl.GetLength(1)]; // создаём одномерный массив из эл-тов 2мерного массива tabl 
+    // длина массива равна кол-ву эл-тов 2мерного массива tabl
+    for (int i = 0; i < result.Length; i++)
+    {
+        for (int j = 0; j < tabl.GetLength(0); j++) // перебираем строки
+        {
+            for (int q = 0; q < tabl.GetLength(1); q++)
+            {
+                result[i] = tabl[j, q]; // получаем новый массив
+            }
+        }
+    }
+}
+
+void PrintArray(int[,] tabl) // печать полученного массива
+{
+    for (int i = 0; i < tabl.GetLength(0); i++)
+    {
+        for (int j = 0; j < tabl.GetLength(1); j++)
+        {
+            Console.Write($"{tabl[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+void PrintNumb(int[,] tabl, int numb) // печать искомого эл-та
+{
+    int[] result = new int[tabl.GetLength(0) * tabl.GetLength(1)]; // создаём одномерный массив из эл-тов 2мерного массива tabl 
+    // длина массива равна кол-ву эл-тов 2мерного массива tabl
+    for (int i = 0; i < result.Length; i++)
+    {
+        for (int j = 0; j < tabl.GetLength(0); j++) // перебираем строки
+        {
+            for (int q = 0; q < tabl.GetLength(1); q++)
+            {
+                if(i == numb)
+                {
+                    result[i] = tabl[j, q]; // получаем новый массив
+                    Console.WriteLine(" -> " + result[numb]);
+                }
+                else Console.WriteLine(numb + " -> Такой позиции в масиве нет. Введите число от 0 до 15");
+            }
+        }
+    }
+        
+    Console.WriteLine();
+}
+
+int numb = GetIndex("Введите позицию элемента от 0 до 15"); // запрос ввода индекса
+int[,] tablic = new int[4, 4];
+FillArray(tablic);
+GetNumb(tablic, numb);
+PrintArray(tablic);
+PrintNumb(tablic, numb);
+
+
---
+++ Seminar7/DZ/Zadacha2_Sred_arifm_2massiv/Program.cs
@@ -0,0 +1,70 @@
+﻿/* Задача 52. Задайте двумерный массив из целых чисел.
+Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.  */
+
+void FillArray(int[,] tabl)
+{
+    for (int i = 0; i < tabl.GetLength(0); i++)
+    {
+        for (int j = 0; j < tabl.GetLength(1); j++)
+        {
+            tabl[i, j] = new Random().Next(1, 100); // заполняем массив случайными числами из полуинтервала (1, 100]
+        }
+    }
+}
+
+void PrintArray(int[,] tabl) // печать полученного массива
+{
+    for (int i = 0; i < tabl.GetLength(0); i++)
+    {
+        for (int j = 0; j < tabl.GetLength(1); j++)
+        {
+            Console.Write($"{tabl[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+double[] SrArifm(int[,] tabl)
+{
+    double[] result = new double[tabl.GetLength(1)]; // создаём одномерный массив из сред арефм-х по каждому столбцу 
+    // длина массива равна кол-ву столбцов
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = 0; // задаём переменную суммы эл-тов в столбце
+        for (int j = 0; j < tabl.GetLength(0); j++) // перебираем элементы в столбце
+        {
+            result[i] += tabl[j, i]; // определяем сумму эл-тов в столбце
+        }
+        result[i] /= tabl.GetLength(0); // делим сумму на кол-во эл-тов в столбце
+    }
+    return result; // получаем результирующий массив из средн арефм по столбцам
+}
+
+void PrintRez(int[,] tabl)
+{
+    Console.Write("Среднее арифметическое каждого столбца: ");
+    double[] result = new double[tabl.GetLength(1)];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = 0; // задаём переменную суммы эл-тов в столбце
+        for (int j = 0; j < tabl.GetLength(0); j++) // перебираем элементы в столбце
+        {
+            result[i] += tabl[j, i]; // определяем сумму эл-тов в столбце
+        }
+        result[i] /= tabl.GetLength(0);
+        Console.Write($"{result[i]} | ");
+    }
+    Console.WriteLine();
+}
+
+int[,] tablic = new int[4, 5];
+FillArray(tablic);
+PrintArray(tablic);
+double[] rez = SrArifm(tablic);
+PrintRez(tablic);
