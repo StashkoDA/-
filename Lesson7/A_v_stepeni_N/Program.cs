76783dd1dc2aa2716e737eb4c06d5176186366f4
Dmitrii_Stashko <1028047@mail.ru>
2022-11-29 16:43:38 +1000

    Добавил задачу A_v_stepeni_N

---
+++ Lesson7/A_v_stepeni_N/Program.cs
@@ -0,0 +1,23 @@
+﻿// Возведение числа А в степень N
+
+// Без рекурсии:
+int PowerFor(int a, int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        result *= a;
+    }
+    return result;
+}
+
+// С рекурсией:
+int PowerRec(int a, int n)
+{
+    if (n == 0) return 1;
+    else return a * PowerRec(a, n - 1);
+    // или: return n == 0 ? 1 : a * PowerRec(a, n - 1);
+}
+
+Console.WriteLine(PowerFor(2, 10));
+Console.WriteLine(PowerRec(2, 10));
