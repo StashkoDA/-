5e3d1fc302cb7655613c07e5a300e78d982a22c0
Dmitrii_Stashko <1028047@mail.ru>
2022-11-29 15:15:28 +1000

    Добавил задачу Rekursia_ot_A_do_B

---
+++ Lesson7/Rekursia_ot_A_do_B/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+Собрать строчку с числами от А до Б
+*/
+
+// Без рекурсии:
+/*
+string NumbersFor(int a, int b)
+{
+    string result = String.Empty;
+    for (int i = a; i <= b; i++)
+    {
+        result += $"{i} ";
+    }
+    return result;
+}
+
+// С рекурсией:
+string NumbersRec(int a, int b)
+{
+    if (a <= b) return $"{a} " + NumbersRec(a +1, b);
+    else return String.Empty;
+}
+
+Console.WriteLine(NumbersFor(1, 10));
+Console.WriteLine(NumbersRec(1, 10));
+*/
+// От большего к меньшему:
+// Без рекурсии:
+string NumbersFor(int a, int b)
+{
+    string result = String.Empty;
+    for (int i = b; i >= a; i--)
+    {
+        result += $"{i} ";
+    }
+    return result;
+}
+
+// С рекурсией:
+string NumbersRec(int a, int b)
+{
+    if (a <= b) return NumbersRec(a +1, b) + $"{a} ";
+    else return String.Empty;
+}
+
+Console.WriteLine(NumbersFor(1, 10));
+Console.WriteLine(NumbersRec(1, 10));
