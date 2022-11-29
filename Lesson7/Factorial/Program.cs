947d8db094d0e57cf6a0c357f42360a32fa9bdf1
Dmitrii_Stashko <1028047@mail.ru>
2022-11-29 15:44:08 +1000

    Добавил задачу Factorial

---
+++ Lesson7/Factorial/Program.cs
@@ -0,0 +1,22 @@
+﻿// Факториал числа N
+
+// Без рекурсии:
+int FactorialFor(int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
+
+// С рекурсией:
+int FactorialRec(int n)
+{
+    if (n == 1) return 1;
+    else return n * FactorialRec(n - 1);
+}
+
+Console.WriteLine(FactorialFor(10));
+Console.WriteLine(FactorialRec(10));
