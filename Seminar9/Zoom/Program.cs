3b49702bedbbfa72cc447c118211e6b5797d2747
Dmitrii_Stashko <1028047@mail.ru>
2023-01-05 16:47:01 +1000

    Добавил задачу на масштабирование массива Seminar9/Zoom/

---
+++ Seminar9/Zoom/Program.cs
@@ -0,0 +1,40 @@
+﻿// Увеличить масштаб массива
+
+int [,] Resize(int[,] f, int c) // Функция увеличения двумерного массива f с кратностью масштаба "с".
+{
+    int rows = f.GetLength(0);
+    int columns = f.GetLength(1);
+    int[,] res = new int[rows * c, columns * c];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            for (int k = i * c; k < i * c + c; k++)
+            {
+                for (int l = j * c; l < j * c + c; l++)
+                {
+                    res[k, l] = f[i, j];
+                }
+            }
+        }
+    }
+    return res;
+}
+
+int[,] f = new int[,] {  // Исходный массив f.
+    {0,1,0},
+    {1,0,1},
+    {0,0,1},
+};
+
+int[,] r = Resize(f, 3);
+
+for (int i = 0; i < r.GetLength(0); i++)
+{
+    for (int j = 0; j < r.GetLength(1); j++)
+    {
+        Console.Write($"{r[i, j]}");
+    }
+    Console.WriteLine();
+}
